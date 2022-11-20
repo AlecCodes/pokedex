cfce8c53353b4cea914993e26e2f1aedfb0982b0
Alec Hannaford <alec322@gmail.com>
2022-11-20 10:49:44 -0800

    linked app js and adding a button w event listener for filters it only clears the div right now

---
+++ public/app.js
@@ -0,0 +1,8 @@
+const picgrid = document.getElementById("images")
+const button = document.getElementById("clear")
+
+function clearer(){picgrid.replaceChildren()};
+
+button.addEventListener("click",clearer)
+
+ 
